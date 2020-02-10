1b975b9ce03e7146d31b831bed7d1a8fc4e7e3bc
janejiraAroon <janejira885@gmail.com>
2020-02-10 19:26:26 +0700

    edit url mapping for customers app

--- romeo-back/backend/urls.py
+++ romeo-back/backend/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from customers.views import customers
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('customers/', include('customers.urls'))
+    path('customers/', customers)
 ]
