38c73ed88bddd016d617458ac5211ed9f3ca8df9
Xintien <xintien.pei@gmail.com>
2020-02-11 14:48:12 +0700

    back:add auth api

--- romeo-back/photographers/admin 2.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import PhotographerInfo, Photo, AvailTime, Equipment
-
-admin.site.register(PhotographerInfo)
-admin.site.register(Photo)
-admin.site.register(AvailTime)
-admin.site.register(Equipment)
-
-# Register your models here.
