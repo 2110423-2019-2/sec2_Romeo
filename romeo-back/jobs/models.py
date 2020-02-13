6a9e81bc255d97289e052df4a8bf040384ce92d0
Xintien <xintien.pei@gmail.com>
2020-02-13 09:20:19 +0700

    add and edit api for user types

--- romeo-back/jobs/models.py
+++ romeo-back/jobs/models.py
@@ -1,4 +1,11 @@
 from django.db import models
-
+from customers.models import CustomerInfo
+from photographers.models import PhotographerInfo
 # Create your models here.
-# TODO create model
+
+# JOB_STATUS_CHOICES=[('TEST','Test')]
+
+# class JobInfo(models.Model):
+#     JobCustomer = models.ForeignKey(CustomerInfo, on_delete=models.CASCADE)
+#     JobPhotographer = models.ForeignKey(PhotographerInfo, on_delete=models.CASCADE)
+#     JobStatus = models.CharField(choices=JOB_STATUS_CHOICES,max_length=4)
