5461d0a0ec90d6ee469df8c67936aba69106dd1d
Dheeyos <manutd.leo@hotmail.com>
2020-03-26 16:07:20 +0700

    Backend : rateJob 0-10

--- romeo-back/reviews/models.py
+++ romeo-back/reviews/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from jobs.models import JobInfo
 # Create your models here.
 # TODO create model
@@ -6,8 +7,9 @@ from jobs.models import JobInfo
 
 class ReviewInfo(models.Model):
     reviewJob = models.OneToOneField(JobInfo, limit_choices_to={'JobStatus': 'Closed'}, primary_key=True, on_delete=models.CASCADE)
-    reviewDetail = models.TextField()
-    # rateJob  = models.
+    reviewDetail = models.TextField(null=True, blank=True)
+    rateJob  = models.IntegerField(validators=[MinValueValidator(0),
+                                       MaxValueValidator(10)])
     def __str__(self):
         return self.reviewDetail
 
