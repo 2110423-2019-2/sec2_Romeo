cfa3082c963b68856b77132b5cb1bf72d03a8c83
NonthakornK <lak231928@gmail.com>
2020-04-27 16:50:05 +0700

    Fix FavouriteTest

--- romeo-front/cypress/integration/testFrontEnd/FavouriteTest.js
+++ romeo-front/cypress/integration/testFrontEnd/FavouriteTest.js
@@ -2,7 +2,8 @@ import { wait } from "@testing-library/react"
 
 describe('Test Favourite Photographer', function() {
 
-    it('TC 5.1-01 Add favourite photographer', function() {
+    beforeEach(function() { 
+
         cy.visit('localhost:3000')
         cy.title().should('eq', 'Photo-Bro')
         cy.get('button.mr-2').click()
@@ -14,25 +15,37 @@ describe('Test Favourite Photographer', function() {
         })
         cy.wait(1000)
         cy.contains('Find your photographer').should('be.visible')
+    })
+
+    it('TC 5.1-01 Add favourite photographer', function() {
+        
+        cy.get('.ant-btn-icon-only').last().click()
+        cy.get('.ant-dropdown-menu-item').eq(1).click()
+        cy.contains('You do not have any favorite photographers.').should('be.visible')
+        cy.wait(2000)
+        cy.visit('localhost:3000')
         cy.contains('test').should('be.visible')
         cy.visit('http://localhost:3000/profile/test')
         cy.get('.sidebar-profile > .ant-btn').click()  
+        cy.get('.ant-btn-icon-only').last().click()
+        cy.get('.ant-dropdown-menu-item').eq(1).click()
+        cy.contains('test').should('be.visible')
+        cy.wait(2000)
     })
     it('TC 5.1-01 Add favourite photographer', function() {
+       
+        cy.get('.ant-btn-icon-only').last().click()
+        cy.get('.ant-dropdown-menu-item').eq(1).click()
+        cy.contains('test').should('be.visible')
+        cy.wait(2000)
         cy.visit('localhost:3000')
-        cy.title().should('eq', 'Photo-Bro')
-        cy.get('button.mr-2').click()
-        cy.contains('Password')
-        cy.get('form').within(($form) => {
-            cy.get('input').first().type('qwer')
-            cy.get('input').last().type('qwer')
-            cy.get('.ant-btn').click()
-        })
-        cy.wait(1000)
-        cy.contains('Find your photographer').should('be.visible')
         cy.contains('test').should('be.visible')
         cy.visit('http://localhost:3000/profile/test')
         cy.get('.sidebar-profile > .ant-btn').click()  
+        cy.get('.ant-btn-icon-only').last().click()
+        cy.get('.ant-dropdown-menu-item').eq(1).click()
+        cy.contains('You do not have any favorite photographers.').should('be.visible')
+        cy.wait(2000)
     })
     
 })
