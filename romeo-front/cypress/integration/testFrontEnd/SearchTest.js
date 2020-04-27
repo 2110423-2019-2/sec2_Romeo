970bbef541cbf497998ddcb6fafb8e247208ae08
NonthakornK <lak231928@gmail.com>
2020-04-27 17:23:23 +0700

    SearchTest

---
+++ romeo-front/cypress/integration/testFrontEnd/SearchTest.js
@@ -0,0 +1,10 @@
+describe('Test Search by filter', function() {
+    it('TC 6.1-01 Search with any length and filters', function() {
+        cy.visit('localhost:3000')
+        cy.title().should('eq', 'Photo-Bro')
+        cy.get('input').first().type('Peter')
+        cy.get('input').first().type('{enter}')
+        cy.wait(5000)
+        cy.contains('No Results').should('be.visible')
+    })
+})
